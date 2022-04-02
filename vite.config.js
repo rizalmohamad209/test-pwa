6184e909124887cf0a612eb545c99909e1cf68ac
rizalmohamad209 <rizalmohamad209@gmail.com>
2022-04-02 19:31:43 +0700

    config pwa

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,32 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
-  VitePWA({})]
+  VitePWA({
+
+    includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+    manifest: {
+      name: 'PPDB Tegal',
+      short_name: 'PPDB',
+      description: 'Description of your app',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        }
+      ]
+    }
+  })]
 })
