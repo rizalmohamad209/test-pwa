93933499ca577cf1c4243acb751637fbf062a8a0
rizalmohamad209 <rizalmohamad209@gmail.com>
2022-04-02 19:56:19 +0700

    config service worker

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(),
   VitePWA({
 
-    includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+    includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt'],
     manifest: {
       name: 'PPDB Tegal',
       short_name: 'PPDB',
